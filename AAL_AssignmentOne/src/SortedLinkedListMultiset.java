16e8bc0671cfecf96cd126df9ee6a025a28d3472
Adrian Nacor <30950146+3595871@users.noreply.github.com>
2017-08-21 21:43:12 +1000

    Implemented add function
    Double check it

--- AAL_AssignmentOne/src/SortedLinkedListMultiset.java
+++ AAL_AssignmentOne/src/SortedLinkedListMultiset.java
@@ -1,30 +1,102 @@
 import java.io.PrintStream;
 import java.util.*;
 
+
 public class SortedLinkedListMultiset<T> extends Multiset<T> {
 	
 	protected Node mHead;
 	protected Node mTail;
 	protected Node mCurrent;
+	protected int mLength;
 	
 	public SortedLinkedListMultiset() {
 		// Implement me!
 		mHead = null;
 		mTail = null;
 		mCurrent = null;
+		mLength = 0;
 	} // end of SortedLinkedListMultiset()
 
 	public void add(T item) {
 		// Implement me!
-		Node mNode = new Node(item);
-		
-		if(mHead == null){
-			//If head is empty then list is empty
-			mHead = mNode;
-			mTail = mNode;
-		}else if(search(item) != 0){
-			//If head is not empty and there is a duplicate item already in the list
+		Node newNode = new Node(item);
+
+		if (mHead == null) {
+			// If head is empty, then the list is empty
+			mHead = newNode;
+			mTail = newNode;
+			mLength++;
+		} else if (search(item) != 0) {
+			// Item already exists in the list - increase count
+			Node currNode = getNode(item);
+			currNode.increaseCount();
+		} else {
 			
+			// item doesn't exist, compare the first character to all the nodes available
+			
+			Node currNode = mHead;
+						
+			while(currNode != null)
+			{
+							
+				if(((String) newNode.getValue()).charAt(0) > ((String) currNode.getValue()).charAt(0))
+				{
+								
+						currNode = currNode.getNext();
+				}
+				else
+				{
+					
+					// Checks the previous node of the currNode, if its empty then it is the head 
+					
+					if(currNode.getPrev() == null)
+					{
+						// insert at head
+						
+						currNode.setPrev(newNode);
+						
+						newNode.setNext(currNode);
+						newNode.setPrev(null);
+						mLength++;
+						
+						// if the next node of the currNode is null then assign it as tail
+						
+						if(currNode.getNext() == null)
+						{
+							mTail = currNode;
+						}
+							
+					}
+					else if(currNode.getPrev() != null && currNode.getNext() != null)
+					{
+						// insert in the middle
+						
+						newNode.setPrev(currNode.getPrev()); 
+						newNode.setNext(currNode);
+						currNode.getPrev().setNext(newNode);
+						currNode.setPrev(newNode);
+						mLength++;	
+						
+						
+					}
+				
+				}
+			
+			  
+			}
+			
+			if(currNode == null)
+			{
+				// insert at the end
+				
+				newNode.mPrev = mTail;
+				newNode.mNext = null;
+				mTail.mNext = newNode;
+				mTail = newNode;
+				mLength++;
+			}
+			
+		
 		}
 	} // end of add()
 
@@ -46,6 +118,22 @@ public class SortedLinkedListMultiset<T> extends Multiset<T> {
 	public void print(PrintStream out) {
 		// Implement me!
 	} // end of print()
+	
+	public Node getNode(T item) {
+		//Finds and returns node that has the exact item
+		Node currNode = mHead;
+		Node tempNode = null;
+		while (currNode != null) {
+			if (((String) item).compareTo((String) currNode.getValue()) == 0) {
+				tempNode = currNode;
+				currNode = null;
+			} else {
+				currNode = currNode.getNext();
+			}
+		}
+		return tempNode;
+
+	}
 
 	private class Node {
 		/** Stored item of node. */
@@ -63,6 +151,10 @@ public class SortedLinkedListMultiset<T> extends Multiset<T> {
 			mNext = null;
 			mCount = 1;
 			
+		}
+		
+		public T getValue() {
+			return mItem;
 		}
 
 		public void setNext(Node next) {
