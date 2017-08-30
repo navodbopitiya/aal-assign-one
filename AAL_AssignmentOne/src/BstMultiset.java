ab9ec74c15b8ab88c7f29dd36bb236730f1375c1
Navod Bopitiya <s3617221@student.rmit.edu.au>
2017-08-30 20:21:12 +1000

    Fixed : Fixed all methods of bst

--- AAL_AssignmentOne/src/BstMultiset.java
+++ AAL_AssignmentOne/src/BstMultiset.java
@@ -1,83 +1,184 @@
 import java.io.PrintStream;
 import java.util.*;
 
-public class BstMultiset<T> extends Multiset<T>
-{
+public class BstMultiset<T> extends Multiset<T> {
 	private Node mRoot;
-	
+
 	public BstMultiset() {
-		// Implement me!
 		mRoot = null;
 	} // end of BstMultiset()
 
 	public void add(T item) {
-		// Implement me!
+		mRoot = insert(mRoot, item);
 	} // end of add()
 
+	public Node insert(Node node, T item) {
+
+		if (node == null) {
+			Node newNode = new Node(item);
+			return newNode;
+		}
+		if (((String) item).compareTo((String) node.getItem()) > 0) {
+			// add to the right
+			node.setRightChild(insert(node.getRightChild(), item));
+
+		} else if (((String) item).compareTo((String) node.getItem()) < 0) {
+			// add to the left
+			node.setLeftChild(insert(node.getLeftChild(), item));
+		} else {
+			// Item already exists in the tree, increase count.
+			node.increaseCount();
+		}
+		return node;
+	}// end of insert
+
+	public Node find(Node node, T item) {
+
+		while (node != null) {
+			if (((String) item).compareTo((String) node.getItem()) == 0) {
+				return node;
+			} else if (((String) item).compareTo((String) node.getItem()) < 0) {
+				node = node.getLeftChild();
+			} else {
+				node = node.getRightChild();
+			}
+		}
+		return null;
+	}// end of find
 
 	public int search(T item) {
-		// Implement me!
-
-		// default return, please override when you implement this method
-		return 0;
+		Node node = find(mRoot, item);
+		if (node == null) {
+			return 0;
+		} else {
+			return node.getCount();
+		}
 	} // end of add()
 
-
 	public void removeOne(T item) {
-		// Implement me!
+		Node node = find(mRoot, item);
+
+		if (node != null) {
+			node.decreaseCount();
+			if (node.getCount() == 0) {
+				// If Count is zero, we need to remove entire node
+				removeNode(mRoot, item);
+			}
+		}
 	} // end of removeOne()
-	
-	
+
 	public void removeAll(T item) {
-		// Implement me!
+		Node node = find(mRoot, item);
+		if (node != null) {
+			removeNode(mRoot, item);
+		}
 	} // end of removeAll()
 
+	public void removeNode(Node node, T item) {
+		Node leftNode;
+		Node rightNode;
+		Node parentNode = null;
+		int check = 0;
+
+		while (check != 1) {
+			if (((String) item).compareTo((String) node.getItem()) == 0) {
+				if (parentNode == null) {
+					if (node.getRightChild() != null) {
+						rightNode = node.getRightChild();
+						if (node.getLeftChild() != null) {
+							mRoot = node.getLeftChild();
+							/* insert right node into left subtree */
+							insert(mRoot, rightNode.getItem());
+						} else {
+							mRoot = node.getRightChild();
+						}
+					} else if (node.getLeftChild() != null) {
+						mRoot = node.getLeftChild();
+					} else {
+						mRoot = null;
+					}
+
+				} else {
+					/* item is in parent's left sub-tree */
+					if (((String) item).compareTo((String) node.getItem()) < 0) {
+
+						rightNode = node.getRightChild();
+						parentNode.setLeftChild(node.getLeftChild());
+						if (rightNode != null) {
+							insert(mRoot, rightNode.getItem());
+						}
+						/* item is in parent's right sub-tree */
+					} else if (((String) item).compareTo((String) node.getItem()) > 0) {
+
+						leftNode = node.getLeftChild();
+						parentNode.setRightChild(node.getRightChild());
+						if (leftNode != null) {
+							insert(mRoot, leftNode.getItem());
+						}
+					}
+				}
+				check = 1;
+			} else if (((String) item).compareTo((String) node.getItem()) < 0) {
+				parentNode = node;
+				node = node.getLeftChild();
+			} else {
+				parentNode = node;
+				node = node.getRightChild();
+			}
+		}
+	}// end of removeNode
 
 	public void print(PrintStream out) {
-		// Implement me!
+		inOrder(mRoot, out);
 	} // end of print()
 
-	public class Node{
-		/* Stored item of node */
+	public void inOrder(Node root, PrintStream out) {
+		if (root == null) {
+			return;
+		}
+		inOrder(root.getLeftChild(), out);
+		out.println(root.getItem() + printDelim + root.getCount());
+		inOrder(root.getRightChild(), out);
+	}// end of inOrder
+
+	public class Node {
 		private T mItem;
-		/* Reference to left child sub-tree */
-		private Node mLeft;
-		/* Reference to right child sub-tree */
-		private Node mRight;
-		/* Count of duplicate items in node */
 		private int mCount;
-		
-		public Node(T item){
+		private Node mRightChild;
+		private Node mLeftChild;
+
+		public Node(T item) {
 			mItem = item;
-			mLeft = null;
-			mRight = null;
-			mCount = 1; 
-		}
+			mCount = 1;
+			mRightChild = null;
+			mLeftChild = null;
 
-		public Node getLeft() {
-			return mLeft;
-		}
-
-		public void setLeft(Node mLeft) {
-			this.mLeft = mLeft;
-		}
-
-		public Node getRight() {
-			return mRight;
-		}
-
-		public void setRight(Node mRight) {
-			this.mRight = mRight;
 		}
 
 		public T getItem() {
 			return mItem;
 		}
 
+		public Node getLeftChild() {
+			return mLeftChild;
+		}
+
+		public void setLeftChild(Node node) {
+			mLeftChild = node;
+		}
+
+		public Node getRightChild() {
+			return mRightChild;
+		}
+
+		public void setRightChild(Node node) {
+			mRightChild = node;
+		}
+
 		public int getCount() {
 			return mCount;
 		}
-		
+
 		public void increaseCount() {
 			mCount++;
 		}
@@ -86,5 +187,6 @@ public class BstMultiset<T> extends Multiset<T>
 			mCount--;
 		}
 
-	}
+	}// end of inner class Node
+
 } // end of class BstMultiset
