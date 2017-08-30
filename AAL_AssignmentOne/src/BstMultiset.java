f1353c0b71b4103d9b2101047091aeb005b7ee8f
Adrian Nacor <30950146+3595871@users.noreply.github.com>
2017-08-30 15:13:15 +1000

    Bunch of stuff

--- AAL_AssignmentOne/src/BstMultiset.java
+++ AAL_AssignmentOne/src/BstMultiset.java
@@ -3,20 +3,63 @@ import java.util.*;
 
 public class BstMultiset<T> extends Multiset<T>
 {
+	
+	public static final int EMPTY_TREE = -1;
+
+    /** Root node of the tree. */
+	protected Node mRoot;
+
+
 	public BstMultiset() {
 		// Implement me!
+		mRoot = null;
 	} // end of BstMultiset()
 
 	public void add(T item) {
 		// Implement me!
+		Node newNode = new Node(item);
+		
+		if(mRoot == null) {
+			mRoot = newNode;
+			return;
+		} 
+		
+		Node current = mRoot;
+		Node parent = null;
+		
+		while(true) {
+			
+			parent = current;
+								
+			if(((String) item).compareTo((String)current.getValue()) > 0) {
+				
+				current = current.mRightChild;
+				
+				if(current==null){
+					parent.mLeftChild = newNode;
+					return;
+				}
+				
+			}
+		}
+			
 	} // end of add()
 
 
 	public int search(T item) {
 		// Implement me!
-
-		// default return, please override when you implement this method
-		return 0;
+		Node current = mRoot;
+		while(current!=null){
+			if(current.mItem==item){
+				// found the node
+			}else if(current.mItem>item){
+				current = current.mLeftChild;
+			}else{
+				current = current.mRightChild;
+			}
+		}
+			
+		return 0; // return count
 	} // end of add()
 
 
@@ -33,5 +76,60 @@ public class BstMultiset<T> extends Multiset<T>
 	public void print(PrintStream out) {
 		// Implement me!
 	} // end of print()
+	
+	
+	
+	class Node
+	{
+		  	private T mItem;
+	        /** Reference to left child. */
+	        public Node mLeftChild;
+	        /** Reference to right child. */
+	        public Node mRightChild;
+
+
+	        /**
+	         * Constructor.
+	         * @param <T>
+	         *
+	         * @param key Key to store in node.
+	         */
+	        public Node(T item) {
+	                mItem = item;
+	                mLeftChild = null;
+	                mRightChild = null;
+	        }
+
+
+	        /**
+	         *
+	         * @return Key stored in node.
+	         */
+	        public T getValue() {
+	            return mItem;
+	        } // end of getKey()
+
+
+	        /**
+	         *
+	         * @return Reference to left child of node.
+	         */
+	        public Node leftChild() {
+	            return mLeftChild;
+	        } // end of leftChild()
+
+
+	        /**
+	         *
+	         * @return Reference to right child of node.
+	         */
+	        public Node rightChild() {
+	            return mRightChild;
+	        } // end of rightChild()
+
+	} // end of class Node
+
 
 } // end of class BstMultiset
+
+
